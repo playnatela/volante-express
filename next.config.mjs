583f1de64577d47fdd887f62bf895f5974b544fa
Wildson <wildsoncaldeira@gmail.com>
2026-01-31 18:33:24 -0300

    Transformando em PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from 'next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // Desativa PWA enquanto você programa no PC
+});
+
 const nextConfig = {
-  /* config options here */
+  // Outras configs se necessário
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
